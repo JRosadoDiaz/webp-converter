c42e91a85e19a687ee98a6343c11b58077a37856
JRosadoDiaz <jose.rosado853@gmaill.com>
2022-06-04 08:38:41 -0600

    Log window now scrolls to bottom after conversions

--- main.py
+++ main.py
@@ -141,6 +141,8 @@ class MainWindow(QMainWindow):
                                                     self.directoryLine.text() + '/WEBP\n')
                 self.convertLog.insertPlainText("Conversions Finished!\n")
 
+            self.convertLog.verticalScrollBar().setValue(
+                self.convertLog.verticalScrollBar().maximum())
             self.verifyCheck = False
 
     def findFiles(self):
